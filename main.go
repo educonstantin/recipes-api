6ef850255f8483673f23091238f8dd6eb45ad665
Eduardo Constantin <educonstantin@gmail.com>
2022-07-23 11:29:17 -0300

    "list recipes endpoint"

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"recipes-api/model"
 	"time"
@@ -13,6 +15,8 @@ var recipes []model.Recipe
 
 func init() {
 	recipes = make([]model.Recipe, 0)
+	file, _ := ioutil.ReadFile("recipes.json")
+	_ = json.Unmarshal([]byte(file), &recipes)
 }
 
 func NewRecipeHandler(c *gin.Context) {
@@ -27,8 +31,13 @@ func NewRecipeHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipe)
 }
 
+func ListRecipesHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, recipes)
+}
+
 func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
+	router.GET("/recipes", ListRecipesHandler)
 	router.Run()
 }
